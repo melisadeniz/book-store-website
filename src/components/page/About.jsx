e9a4eaf47698730f264588816c338caaff7190bc
melisadeniz <melisadeniz95@gmail.com>
2022-01-04 03:11:11 +0300

    detail

--- src/components/page/About.jsx
+++ src/components/page/About.jsx
@@ -4,6 +4,19 @@ function About(props) {
   return <>
   <div class="col-sm-12">
   <h1>About</h1>
+  <h4>Project Details</h4>
+   <p>
+    In this assignment, I tried to create a DR Website Clone. 
+    I used an API that does not require authentication. 
+    I took the data from there and created a responsive website where all products can be seen,
+    each product detail can be viewed and books can be searched by title.
+   </p>
+   <h6>Technologies I used</h6>
+   <ul>
+     <li>React</li>
+     <li>React Router Dom</li>
+     <li>Bootstrap</li>
+   </ul>
   </div>
   </>
   }
